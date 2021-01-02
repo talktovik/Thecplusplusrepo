bbe4feff3e369e41bb14be5a23014e2959474934
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2021-01-02 14:30:57 +0530

    Still Learning

---
+++ CPP_small_Programs/Function_small__.cpp
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<iostream.h>
+int add(int,int);
+void main()
+{
+clrscr();
+int a,b;
+cout << "Enter the Number";
+cin>>a>>b;
+cout<<"sum is "<< add(a,b);
+getch();
+}
+int add(int x,int y )
+{
+return(X+y);
+}
---
+++ CPP_small_Programs/Square.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+void main(){
+	clrscr();
+	int x;
+	cout << "Enter A number"<< endl;
+	cin >> x;
+	int s = x*x;
+	cout << "square of "<< x << " is "<< s;
+	getch();
+}
