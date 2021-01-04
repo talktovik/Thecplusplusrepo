0f9f1c11684bf47d1f74f9cfa8418df35c8e7dbf
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2021-01-04 10:34:51 +0530

    Day2 part1

---
+++ CPP_small_Programs/classes_and_structure.cpp
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<iostream.h>
+struct complex{
+private:
+int a,b;
+public:
+void set_data (intx,inty){
+a=x;b=y;}
+void show data(){
+cout<<"\na"<<a<<"b"<<b;
+};
+void main(){
+clrscr();
+c1.set_data(3,4);
+c1.show_data();
+getch();
+}
+
+}
