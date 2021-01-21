086f7bf2ed389123edd964e805a0c5f2d2d0e9c7
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2021-01-21 12:15:21 +0530

    commit2

--- C++Fromcherno/Source.cpp
+++ C++Fromcherno/Source.cpp
@@ -1,6 +1,12 @@
 //Notes:
 //Only Cpp Files Compiles
 //compile shortcut = (((control + F7)))
+
+
+//Compilation can be done via decleration because compiler sort of trust us, But Linker's have a work to connect wires among the functions.
+// So the linker gives the final flag for executable code.
+
+// compiler actually produces the obj file.
 #include <iostream>
 
 //[const char* message]
@@ -9,8 +15,11 @@
 void log(const char* message);   //this is something decleration...
 //infact we are not even providing the parameters of the function here.
 
+int multiply(int x, int y);
 int main() {
+//const char* Multiply;
 	
-	log("hello world");
+	 log("Multiply");
+	std::cout << multiply(2,3) << std::endl;
 	std::cin.get();
 }
--- C++Fromcherno/log.cpp
+++ C++Fromcherno/log.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
-void log(const char* message) {  //this is actually the definition of the function or say the body of the function.
+//static means that this function is called in this file only, Scope is limited so if you call it anywhere it will show you link error.
+/*static*/ void  log(const char* message) {  //this is actually the definition of the function or say the body of the function.
 	std::cout << message << std::endl;
+
 }
